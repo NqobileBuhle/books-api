fc20e172e7de4cedb17c88cb712a85866501f1dd
NqobileBuhle <139454693+NqobileBuhle@users.noreply.github.com>
2024-08-22 15:52:23 +0200

    New api

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import SearchForm from '../SearchForm/SearchForm'
+
+const Header = () => {
+  return (
+    <div className=''>
+      <header className=''>
+        <div>
+            
+        </div>
+        <Navbar/>
+
+      </header>
+    </div>
+  )
+}
+
+export default Header
